57281f0fd7f49cecc867eb4d5bdb15b804fa4e93
Corey Rose <71119474+crose1121@users.noreply.github.com>
2026-01-12 19:58:10 -0500

    JPA Annotations
    Added JPA Annotations for 2 tables, and a many-to-many relationship between them.

--- src/main/java/com/project/one/spring_7_webapp/domain/Book.java
+++ src/main/java/com/project/one/spring_7_webapp/domain/Book.java
@@ -1,9 +1,8 @@
 package com.project.one.spring_7_webapp.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.util.Set;
 
 @Entity
 public class Book {
@@ -12,6 +11,15 @@ public class Book {
     private Long id;
     private String title;
     private String isbn;
+
+    @ManyToMany
+    @JoinTable(
+            name = "author_book",
+            joinColumns = @JoinColumn(name = "book_id"),
+            inverseJoinColumns = @JoinColumn(name = "author_id")
+    )
+
+    private Set<Author> authors;
 
     public Long getId() {
         return id;
@@ -35,5 +43,13 @@ public class Book {
 
     public void setIsbn(String isbn) {
         this.isbn = isbn;
+    }
+
+    public Set<Author> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(Set<Author> authors) {
+        this.authors = authors;
     }
 }
